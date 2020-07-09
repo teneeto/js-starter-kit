34075659e52041b0df393d244f7dd48bfae3eff3
Olei Eto <teneeto@gmail.com>
2020-07-09 11:54:59 +0100

    start test with npm start

---
+++ src/index.test.js
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import jsdom from 'jsdom';
+
+const { JSDOM } = jsdom;
+import fs from 'fs';
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  })
+})
+
+// describe('index.html', () => {
+//   it('should say hello', (done) => {
+//     const index = fs.readFileSync("src/index.html", "utf-8");
+//     jsdom.env(index, function (err, window) {
+//       const h1 = window.document.getElementsByTagName('h1')[0];
+//       expect(h1.innerHTML).to.equal("Hello World");
+//       done();
+//       window.close();
+//     })
+//   })
+// })
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync("src/index.html", "utf-8");
+    const dom = new JSDOM(index);
+    const h1 = dom.window.document.getElementsByTagName('h1')[0];
+    expect(h1.innerHTML).to.equal("Hello World");
+    done();
+    window.close();
+  })
+})
