d8e77e4ba95a5455bd1f93fc80b53717ab3d2606
Olei Eto <teneeto@gmail.com>
2020-07-09 02:43:51 +0100

    configuring webpack for development

---
+++ webpack.config.dev.js
@@ -0,0 +1,16 @@
+import path from 'path';
+
+export default {
+  mode: 'development',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  devtool: 'inline-source-map',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+}
