3bbc538433049b4d295444137712de021be35e18
Olei Eto <teneeto@gmail.com>
2020-07-09 04:30:28 +0100

    add eslint and eswatch

--- buildscripts/srcServer.js
+++ buildscripts/srcServer.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.listen(port, (err) => {
   if (err) {
+    // eslint-disable-next-line no-console
     console.log(err);
   } else {
     open('http://localhost:' + port);
