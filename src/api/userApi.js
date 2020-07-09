77304719bdf4c4970b5e34a947fdf915fdbe2a89
Olei Eto <teneeto@gmail.com>
2020-07-09 15:48:23 +0100

    add data and fetch setup

---
+++ src/api/userApi.js
@@ -0,0 +1,17 @@
+export function getUsers() {
+  return get('users');
+}
+
+function get(url) {
+  return fetch(url).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+  return response.json();
+}
+
+function onError(error) {
+  // eslint-disable-next-line no-console
+  console.log(error);
+
+}
